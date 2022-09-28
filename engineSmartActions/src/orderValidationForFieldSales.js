e26c46f6597c8573ba90b6664c19dc2f42233a78
CiucaBiancaMaria <ciuca.bianca@yahoo.com>
2022-09-28 15:04:17 +0300

    adding order validation smart actions

---
+++ engineSmartActions/src/orderValidationForFieldSales.js
@@ -0,0 +1,26 @@
+function runAction(payload) {
+	try {
+		const { record: order, related } = payload.data;
+		const [account] = related.Account;
+		const isPORequired = account.PORequired__c;
+		const poNumber = order.PoNumber;
+		if (related.Account === undefined) {
+			throw new Error(`This order is not associated with an account.`);
+		}
+		if (isPORequired === undefined) {
+			throw new Error(`No access to PO Required field.`);
+		}
+		if (isPORequired && !poNumber) {
+			throw new Error(`Please fill out the Purchase Order number before completing the order!`);
+		}
+		payload.data.message = `Order Validated`;
+	} catch (error) {
+		payload.data.error = error?.message;
+	}
+	payload.data.updateDeviceData = true;
+	payload.data.updateDeviceData.Order = true;
+	payload.data.updateDeviceData.OrderIem = true;
+	payload.data.reprice = true;
+
+	return payload;
+}
