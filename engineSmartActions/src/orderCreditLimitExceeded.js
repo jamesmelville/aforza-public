3566b02fb895e358b7c927ff3f079e8848ec4a51
Eddy Ferreira <eddy.ferreira@bearingpoint.com>
2022-12-23 13:27:38 +0000

    credit limit fix

--- engineSmartActions/src/orderCreditLimitExceeded.js
+++ engineSmartActions/src/orderCreditLimitExceeded.js
@@ -5,14 +5,14 @@ function runAction(payload) {
     const creditLimit = account.AvailableCredit__c;
     const orderTotal = order.TotalAmount;
     const orderRecordtype = order.OrderRecordTypeName__c;
-    if(orderRecordtype == 'FieldDirectOrder'){
+    if(orderRecordtype === 'FieldDirectOrder'){
       if (creditLimit === null || creditLimit === undefined) {
         throw new Error(" No credit limit available");
       }
       if (orderTotal === null || orderTotal === undefined) {
         throw new Error(" No order total available");
       }
-      if (creditLimit <= 0 || creditLimit < orderTotal) {
+      if (creditLimit < orderTotal) {
         throw new Error(` Credit Limit Exceeded - Current credit: ${creditLimit} and Order Total is ${orderTotal}`);
       }
       payload.data.message = ` Order total ${orderTotal} is within credit
