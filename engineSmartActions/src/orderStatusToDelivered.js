3ac227631ac809d4e1d96454d2c1d003709c4ea8
Eddy Ferreira <eddy.ferreira@bearingpoint.com>
2022-12-28 14:21:14 +0000

    order delivered cls fix

--- engineSmartActions/src/orderStatusToDelivered.js
+++ engineSmartActions/src/orderStatusToDelivered.js
@@ -6,13 +6,13 @@ function runAction(payload) {
     if(orderStatus === 'Pending Delivery'){
        payload.data.Status = 'Delivered';
     }
-    
+    payload.data.message = 'Order Delivered';
   } catch (error) {
     payload.data.error = error?.message;
   }
   payload.data.updateDeviceData = true;
   payload.data.updateDeviceData.Order = true;
-  payload.data.updateDeviceData.OrderIem = true;
+  payload.data.updateDeviceData.OrderItem = true;
   payload.data.reprice = true;
 
   return payload;
