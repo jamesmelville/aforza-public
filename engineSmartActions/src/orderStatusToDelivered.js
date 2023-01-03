b502c3399c8388f810046fe6916652d91ff503a7
Eddy Ferreira <eddy.ferreira@bearingpoint.com>
2023-01-03 13:05:24 +0000

    orderStatusToDelivered fix

--- engineSmartActions/src/orderStatusToDelivered.js
+++ engineSmartActions/src/orderStatusToDelivered.js
@@ -1,6 +1,6 @@
 function runAction(payload) {
   try {
-    const { record: order, related } = payload.data;
+    const { record: order} = payload.data;
 
     if(order.Status === 'Pending Delivery'){
       order.Status = 'Delivered';
