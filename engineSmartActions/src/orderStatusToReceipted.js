cc6de786f797b8c6afa903b1dbe5ceb03955325b
magdalena-stanciu-bpt <63054679+magdalena-stanciu-bpt@users.noreply.github.com>
2023-01-10 23:14:23 +0200

    Add test smart action

---
+++ engineSmartActions/src/orderStatusToReceipted.js
@@ -0,0 +1,19 @@
+function runAction(payload) {
+  try {
+    const { record: order } = payload.data;
+
+    if(order.Status === 'Pending Delivery'){
+      order.Status = 'Receipted';
+      payload.data.message = 'Order Delivered';
+    }
+    
+  } catch (error) {
+    payload.data.error = error?.message;
+  }
+  payload.data.updateDeviceData = true;
+  payload.data.updateDeviceData.Order = true;
+  payload.data.updateDeviceData.OrderItem = false;
+  payload.data.reprice = false;
+
+  return payload;
+}
