f0c9496254df3a97c199eb8354bf5cff5792de80
Svatopluk Sejkora <100215457+SvataSejkoraBearingPoint@users.noreply.github.com>
2023-01-16 18:34:02 +0100

    Update orderStatusToDelivered.js

--- engineSmartActions/public/orderStatusToDelivered.js
+++ engineSmartActions/public/orderStatusToDelivered.js
@@ -1,1 +1,1 @@
-function runAction(e){try{const{record:r}=e.data;r.Status==="Pending Delivery"&&(r.Status="Delivered",e.data.message="Order Delivered")}catch(r){e.data.error=r==null?void 0:r.message}return e.data.updateDeviceData={order:!0,orderItem:!1},e.data.reprice=!1,e}
+function runAction(e){try{const{record:r}=e.data;r.Status==="Pending Delivery"&&(r.Status="Delivered",e.data.message="Order Delivered")}catch(r){e.data.error=r==null?void 0:r.message}return e.data.updateDeviceData={order:!0,orderItem:!0},e.data.reprice=!1,e}
