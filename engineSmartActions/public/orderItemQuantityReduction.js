a5b07e6aad62e914f82e5cb79dfa3c60cd49eb06
Svatopluk Sejkora <100215457+SvataSejkoraBearingPoint@users.noreply.github.com>
2023-01-16 17:24:45 +0100

    Update orderItemQuantityReduction.js

--- engineSmartActions/public/orderItemQuantityReduction.js
+++ engineSmartActions/public/orderItemQuantityReduction.js
@@ -1,5 +1,5 @@
-function runAction(e){e.data.message="The Order Item Quantities have been reduced";let t=e.data.record,i=new Map,d=new Map;if(e.data.related.PricebookEntry.forEach(r=>{i.set(r.Product2Id,r)}),e.data.related.OrderItem.forEach(r=>{d.set(r.Product2Id,r)}),t.OrderItemQuantityAdjusted__c!==!0)for(let r=1;r<11;r++){let a="MissingProduct"+r+"__c",u="MissingQuantity"+r+"__c";e.data.message+=`
-product lookup name `+a+" product quantity "+u,e.data.message+=`
-product lookup name value `+t[a]+" product quantity value "+t[u],t[a]&&t[u]&&d.has(t[a])&&i.has(t[a])&&d.get(t[a]).Quantity-t[u]>=0&&(e.data.message+=`
-Actually in the logic`,d.get(t[a]).Quantity-=t[u],t.OrderItemQuantityAdjusted__c=!0,e.data.message+=`
-`+i.get(t[a]).Name+": "+t[u])}return e.data.updateDeviceData=!0,e.data.updateDeviceData.Order=!0,e.data.updateDeviceData.OrderItem=!0,e.data.reprice=!1,e}
+function runAction(e){e.data.message="The Order Item Quantities have been reduced";let t=e.data.record,i=new Map,d=new Map;if(e.data.related.PricebookEntry.forEach(a=>{i.set(a.Product2Id,a)}),e.data.related.OrderItem.forEach(a=>{d.set(a.Product2Id,a)}),t.OrderItemQuantityAdjusted__c!==!0)for(let a=1;a<11;a++){let r="MissingProduct"+a+"__c",u="MissingQuantity"+a+"__c";if(e.data.message+=`
+product lookup name `+r+" product quantity "+u,e.data.message+=`
+product lookup name value `+t[r]+" product quantity value "+t[u],t[r]&&t[u]&&d.has(t[r])&&i.has(t[r])&&d.get(t[r]).Quantity-t[u]>=0){let n=d.get(t[r]).Quantity-t[u];e.data.message+=`
+Actually in the logic aaaa new amount = `+n+"",d.get(t[r]).Quantity-=t[u],t.OrderItemQuantityAdjusted__c=!0,e.data.message+=`
+`+i.get(t[r]).Name+": "+t[u]}}return e.data.updateDeviceData=!0,e.data.updateDeviceData.Order=!0,e.data.updateDeviceData.OrderItem=!0,e.data.reprice=!1,e}
